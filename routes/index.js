85bd74e698fda7c51b8d27eb7cd2319fe117348a
0xEllison <ellison_hsu@outlook.com>
2022-09-12 16:01:27 +0800

    初始化bootstrap

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'TrustPay' });
 });
-
+router.get('/pay', function (req, res, next) {
+  res.render('pay', { title: 'TrustPay' });
+});
 module.exports = router;
